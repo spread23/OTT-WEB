56cec6ebb475c0b6140574687f042ce32961a89a
= <=>
2024-04-21 11:42:03 -0500

    Adding profile and vacants section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,9 +3,53 @@ import React from 'react'
 import './home.css'
 
 export const Home = () => {
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <>
-            <h1>Home</h1>
+            <div className='home-container'>
+                <div className='form-home'>
+                    <h2 className='home-title'>
+                        Encuentra la compañia que este solicitando tus talentos
+                    </h2>
+                    <form onSubmit={onSubmit} className='form-container'>
+                        <div className='input-container'>
+                            <label className='title-form' htmlFor="name">Nombre<strong>*</strong></label>
+                            <input type="text" name='name' placeholder='Introduce tu nombre...' />
+                        </div>
+
+                        <div className='input-container'>
+                            <label className='title-form' htmlFor="lastname">Apellido<strong>*</strong></label>
+                            <input type="text" name='lastname' placeholder='Introduce tu apellido...' />
+                        </div>
+
+                        <div className='input-container'>
+                            <label className='title-form' htmlFor="country">Pais<strong>*</strong></label>
+                            <input type="text" name='country' placeholder='Introduce tu pais...' />
+                        </div>
+
+                        <div className='input-container'>
+                            <label className='title-form' htmlFor="talents">Talentos<strong>*</strong></label>
+                            <textarea type="text" name='talents' placeholder='introduce tus talentos...' />
+                        </div>
+                        
+                        <label className='label-check title-form' htmlFor="type">Tipo de trabajo</label>
+                        <div className='input-container-check'>
+                            <input type="checkbox" name='remote' />Remoto
+                            <input type="checkbox" name='presencial' />Presencial
+                            <input type="checkbox" name='hibrido' />Hibrido
+                        </div>
+
+                        <div className='input-container'>
+                            <label className='title-form' htmlFor="experience">Años de experiencia<strong>*</strong></label>
+                            <input type="number" name='experience' />
+                        </div>
+
+                        <button className='submit-form' type='submit'>Crear mi perfil</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/vacants/Vacants.jsx
+++ src/components/vacants/Vacants.jsx
@@ -5,7 +5,48 @@ import './vacants.css'
 export const Vacants = () => {
     return (
         <>
-            <h1>Vacantes</h1>
+            <div className='table-container'>
+                <table className='table-vacants'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Estatus</th>
+                            <th>Aplicantes</th>
+                            <th>Fecha de publicacion</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Nombre de la vacante <br/><strong>Empresa</strong></td>
+                            <td className='vacant-status-red'>Convocatoria abierta</td>
+                            <td>14 talentos</td>
+                            <td>19/07/2022</td>
+                            <td><button className='btn-vacant'>Aplicar a la vacante</button></td>
+                            <td><button className='vacant-option'>...</button></td>
+                        </tr>
+
+                        <tr>
+                            <td>Nombre de la vacante <br/><strong>Empresa</strong></td>
+                            <td className='vacant-status-green'>Convocatoria abierta</td>
+                            <td>14 talentos</td>
+                            <td>19/07/2022</td>
+                            <td><button className='btn-vacant'>Aplicar a la vacante</button></td>
+                            <td><button className='vacant-option'>...</button></td>
+                        </tr>
+
+                        <tr>
+                            <td>Nombre de la vacante <br/><strong>Empresa</strong></td>
+                            <td className='vacant-status-red'>Convocatoria abierta</td>
+                            <td>14 talentos</td>
+                            <td>19/07/2022</td>
+                            <td><button className='btn-vacant'>Aplicar a la vacante</button></td>
+                            <td><button className='vacant-option'>...</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
